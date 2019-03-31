bd424ead75b04e13d9ce97096b69f56a9daa8090
Jason Knabl <jason.knabl@gmail.com>
2019-03-31 13:33:56 -0400

    Move yaml spec to a config directory

--- lib/retrodb/parsers/event.rb
+++ lib/retrodb/parsers/event.rb
@@ -7,7 +7,7 @@ module Parsers
 
     property :id, Serial
 
-    column_definitions = YAML::load_file(File.join(__dir__,'retrosheet_database_mapping.yml'))
+    column_definitions = YAML::load_file(File.join(Retrodb::ROOT, 'config', 'retrosheet_database_mapping.yml'))
 
     column_definitions.each do |row_id, data|
       property data['db_column_name'].to_sym, Object.const_get(data['db_column_type'])
