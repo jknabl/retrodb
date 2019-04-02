97498c1833f0e6d6241d22d3208dec0f1d799ebe
Jason Knabl <jason.knabl@gmail.com>
2019-04-01 20:44:36 -0400

    concurrent DB operations when persisting parsed chadwick event files.

--- lib/retrodb/parsers/chadwick_event_file_parser.rb
+++ lib/retrodb/parsers/chadwick_event_file_parser.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require 'thread/pool'
 
 module Parsers
   # Chadwick event file --> database rows
@@ -11,9 +12,15 @@ module Parsers
     end
 
     def parse
+      # pool = Thread.pool(20)
+
       file_paths.each do |path|
-        parse_chadwick_csv_from(path)
+        # pool.process do
+          parse_chadwick_csv_from(path)
+        # end
       end
+
+      # pool.shutdown
     end
 
     private
@@ -21,11 +28,17 @@ module Parsers
     def parse_chadwick_csv_from(path)
       puts "Parsing chadwick CSV #{path}..."
       save_count = 0
+      # Postgres concurrent connections set to 100
+      pool = Thread.pool(90)
       CSV.foreach(path) do |row|
-        database_record_from_csv_row(row)
-        save_count += 1
-        puts "\n   -- Saved #{save_count} records..." if (save_count % 500) == 0
+
+        pool.process do
+          database_record_from_csv_row(row)
+          save_count += 1
+          puts "\n   -- Saved #{save_count} records..." if (save_count % 500) == 0
+        end
       end
+      pool.shutdown
 
       File.delete(path)
     end
